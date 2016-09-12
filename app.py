025f244977c5af96c35e319d3154f53e45db3908
scott <scott@cogolabs.com>
2016-09-12 11:31:16 -0400

    change quote source

--- app.py
+++ app.py
@@ -17,23 +17,21 @@ app = Flask(__name__)
 # text=94070
 # response_url=https://hooks.slack.com/commands/1234/5678}
 
-def get_quote():
-    url = 'http://quotes.rest/qod.json'
-    response = requests.get(url).json()
-    try:
-        quote_of_day = response['contents']['quotes'][0]['quote']
-    except:
-        quote_of_day = "Some days are just bad days... That's all"
-
-    return quote_of_day
-
 
 @app.route("/slack", methods=["POST"], strict_slashes=False)
 def hello():
-    qod = get_quote()
+    url = 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1'
+    response = requests.get(url).json()
+    try:
+        quote_of_day = response[0]['content']
+        # For formatting info, see https://api.slack.com/docs/message-formatting
+        quote_of_day = quote_of_day.replace('&', '&amp;',).replace('<p>', '').replace('</p>', '')
+    except:
+        quote_of_day = "Some days are just bad days... That's all"
+
     response = {
         "text": "Your quote of the day!",
-        "attachments": [{"text": qod}]
+        "attachments": [{"text": quote_of_day}]
     }
     return jsonify(response)
 
